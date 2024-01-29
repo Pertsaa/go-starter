231aa697ed2ce27c982e0479448e27f5d56797ab
Pertsaa <65128229+Pertsaa@users.noreply.github.com>
2024-01-29 09:17:58 +0200

    feat: chi version

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -3,9 +3,10 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"time"
 
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type server struct {
@@ -34,11 +35,14 @@ func Run() error {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	// r.Use(middleware.Timeout(60 * time.Second))
+	r.Use(middleware.Timeout(60 * time.Second))
 
 	s := newServer(r)
 
-	http.ListenAndServe(":8080", s)
+	err := http.ListenAndServe(":8080", s)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
